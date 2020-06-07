7d4b43912f23eee1a6b68fc799e45d68bfedd137
cabraljv <joaovictorcabralsteam@gmail.com>
2020-06-07 04:58:56 -0300

    create the app struct

---
+++ src/config/twitter.js
@@ -0,0 +1,6 @@
+export default {
+    consumer_key:process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret:process.env.TWITTER_CONSUMER_SECRET,
+    access_token:process.env.TWITTER_ACESS_TOKEN,
+    access_token_secret:process.env.TWITTER_ACESS_TOKEN_SECRET
+}
